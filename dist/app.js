0dbe51eb6bc7de6f90933d0074e5f61021814361
Nico Hämäläinen <nico@sizeof.io>
2016-02-26 11:12:35 +0200

    New stuff with gulp too

---
+++ dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Scope = function Scope() {
+  _classCallCheck(this, Scope);
+
+  console.log("hello");
+};
+
+exports.default = Scope;
+//# sourceMappingURL=app.js.map
